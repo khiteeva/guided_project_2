00ea9c8b7792758ce39027037429551cf7c87bff
Caroline C <eixn01@gmail.com>
2025-06-23 10:54:43 -0400

    fetches data and now displays it

--- planet.js
+++ planet.js
@@ -1,45 +1,58 @@
-let name;
+let nameH1;
 let climate;
 let surface_water;
 let diameter;
 let rotation_period;
 let terrain;
 let gravity;
-let standard;
 let orbital_period;
 let population;
 const baseUrl = `http://localhost:9001/api`;
 
 // Runs on page load
 addEventListener('DOMContentLoaded', () => {
-  name = document.querySelector('h1#name');
-  climate = document.querySelector('span#climate');
-  surface_water = document.querySelector('span#surface_water');
-  diameter = document.querySelector('span#diameter');
-  rotation_period = document.querySelector('span#rotation_period');
-  terrain = document.querySelector('span#terrain');
-  gravity = document.querySelector('span#gravity');
-  standard = document.querySelector('span#standard');
-  orbital_period = document.querySelector('span#orbital_period');
-  population = document.querySelector('span#population');
-  const sp = new URLSearchParams(window.location.search)
-  const id = sp.get('id')
-  fetchPlanet(id)
+    nameH1 = document.querySelector('h1#name');
+    climate = document.querySelector('span#climate');
+    surface_water = document.querySelector('span#surface_water');
+    diameter = document.querySelector('span#diameter');
+    rotation_period = document.querySelector('span#rotation_period');
+    terrain = document.querySelector('span#terrain');
+    gravity = document.querySelector('span#gravity');
+    orbital_period = document.querySelector('span#orbital_period');
+    population = document.querySelector('span#population');
+    const sp = new URLSearchParams(window.location.search)
+    const id = sp.get('id')
+    getPlanet(id)
 });
 
-async function fetchPlanet(id) {
-  let characterUrl = `${baseUrl}/planets/${id}`;
-  return await fetch(characterUrl)
-    .then(res => res.json())
+async function getPlanet(id) {
+    let planet;
+    try {
+        planet = await fetchPlanet(id);
+        console.log(planet);
+    }
+    catch (ex) {
+        console.error(`Error reading planet ${id} data.`, ex.message);
+    }
+    renderPlanet(planet);
+
 }
 
-const renderCharacter = character => {
-  document.title = `SWAPI - ${character?.name}`;  // Just to make the browser tab say their name
-  nameH1.textContent = character?.name;
-  heightSpan.textContent = character?.height;
-  massSpan.textContent = character?.mass;
-  birthYearSpan.textContent = character?.birth_year;
-  homeworldSpan.innerHTML = `<a href="/planet.html?id=${character?.homeworld.id}">${character?.homeworld.name}</a>`;
-  const filmsLis = character?.films?.map(film => `<li><a href="/film.html?id=${film.id}">${film.title}</li>`)
-  filmsUl.innerHTML = filmsLis.join("");
+async function fetchPlanet(id) {
+    let planetURL = `${baseUrl}/planets/${id}`;
+    return await fetch(planetURL)
+        .then(res => res.json())
+}
+
+const renderPlanet = planet => {
+    document.title = `SWAPI - ${planet?.name}`;  // Just to make the browser tab say their name
+    nameH1.textContent = planet?.name;
+    climate.textContent = planet?.climate;
+    surface_water.textContent = planet?.surface_water;
+    diameter.textContent = planet?.diameter;
+    rotation_period.textContent = planet?.rotation_period;
+    terrain.textContent = planet?.terrain;
+    gravity.textContent = planet?.gravity;
+    orbital_period.textContent = planet?.orbital_period;
+    population.textContent = planet?.population;
 }
