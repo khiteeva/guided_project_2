e4eec49d1d542401b33bc975f354b1d350e0fc9c
Cecil <cecilho2486@gmail.com>
2025-06-30 15:04:05 -0400

    added first 5 routes

--- server/server.js
+++ server/server.js
@@ -19,8 +19,72 @@ app.use(express.json());
 const PORT = 3000;
 
 
+app.get('/api/characters', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(characters);
+        const chars = await collection.find({}).toArray();
+        res.json(chars);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
+
+app.get('/api/films', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(films);
+        const films_array = await collection.find({}).toArray();
+        res.json(films_array);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
+
 app.get('/api/planets', async (req, res) => {
-    res.status(200).send("Hmmm, something smells... No socks for you! ☹");
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(planets);
+        const planet_array = await collection.find({}).toArray();
+        res.json(planet_array);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
+
+app.get('/api/characters/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(characters);
+        const char = await collection.find({id : id}).toArray();
+        res.json(char);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
+
+app.get('/api/films/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(films);
+        const film = await collection.find({'id' : id}).toArray();
+        console.log(film)
+        res.json(film);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
 });
 
 app.listen(PORT, () => {
