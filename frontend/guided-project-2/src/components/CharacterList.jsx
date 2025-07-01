bb74bb0ec8d1569f49ffd313f6e3713649431f1d
Cecil <cecilho2486@gmail.com>
2025-07-01 13:30:58 -0400

    removed body tag

--- frontend/guided-project-2/src/components/CharacterList.jsx
+++ frontend/guided-project-2/src/components/CharacterList.jsx
@@ -24,23 +24,20 @@ function CharacterList() {
 
     return (
         <>
-            <body>
-                <div>
-                    <h1>Star Wars Universe Lookup</h1>
-                    {/* <label for="searchString">Who you looking for? <span class="small">(Regular expressions are cool
+            <div>
+                <h1>Star Wars Universe Lookup</h1>
+                {/* <label for="searchString">Who you looking for? <span class="small">(Regular expressions are cool
                         here)</span></label>
                     <input id="searchString" oninput="filterCharacters()" autocomplete="off" /> */}
-                </div>
-                {
-                    CharList.map((char) => (
+            </div>
+            {
+                CharList.map((char) => (
 
-                        <button key={char.id}><Link to={`/characters/${char.id}`}>{char.name}</Link></button>
-                    )
+                    <button key={char.id}><Link to={`/characters/${char.id}`}>{char.name}</Link></button>
+                )
 
-                    )
-                }
-            </body>
-
+                )
+            }
         </>
     )
 }
