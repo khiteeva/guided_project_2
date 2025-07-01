3e169e7cc4952cd4ac0c3fd28c4764602ea86b8b
Cecil <cecilho2486@gmail.com>
2025-07-01 13:19:25 -0400

    finished rendering and imported styling

--- frontend/guided-project-2/src/components/Character.jsx
+++ frontend/guided-project-2/src/components/Character.jsx
@@ -1,38 +1,59 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
+import '../site.css'
 
 function Character(){
-    const [Char, setChar] = useState([]);
-    const [Homeworld, setHomeworld] = useState('');
+    const [Char, setChar] = useState({});
+    const [Homeworld, setHomeworld] = useState({});
     const [FilmsList, setFilmsList] = useState([]);
     const { id } = useParams();
 
-    async function fetchData(url) {
-        try {
-            const response = await fetch(url);
-            if (!response.ok) {
-                throw new Error('Data could not be fetched!');
-            }
-            const json_response = await response.json();
-            return json_response;
-        } catch (error) {
-            console.error('Error fetching characters:', error);
-        }
-    };
-
     useEffect(() => {
-        const data = fetchData(import.meta.env.VITE_SWAPI_API_URL + `/characters/${id}`);
-        setChar(data);
+        async function getData() {
+            try {
+                const response = await fetch(import.meta.env.VITE_SWAPI_API_URL + `/characters/${id}`);
+                if (!response.ok) {
+                    throw new Error('Data could not be fetched!');
+                }
+                const json_response = await response.json();
+                setChar(json_response[0]);
+            } catch (error) {
+                console.error('Error fetching characters:', error);
+            }
+        };
+        getData();
     }, []);
 
     useEffect(() => {
-        const data = fetchData(import.meta.env.VITE_SWAPI_API_URL + `/characters/${id}/planet`);
-        setHomeworld(data);
-    }, '');
+        async function getData() {
+            try {
+                const response = await fetch(import.meta.env.VITE_SWAPI_API_URL + `/characters/${id}/planet`);
+                if (!response.ok) {
+                    throw new Error('Data could not be fetched!');
+                }
+                const json_response = await response.json();
+                setHomeworld(json_response[0]);
+            } catch (error) {
+                console.error('Error fetching characters:', error);
+            }
+        };
+        getData();
+    }, []);
 
     useEffect(() => {
-        const data = fetchData(import.meta.env.VITE_SWAPI_API_URL + `/characters/${id}/films`);
-        setFilmsList(data);
+        async function getData() {
+            try {
+                const response = await fetch(import.meta.env.VITE_SWAPI_API_URL + `/characters/${id}/films`);
+                if (!response.ok) {
+                    throw new Error('Data could not be fetched!');
+                }
+                const json_response = await response.json();
+                setFilmsList(json_response);
+            } catch (error) {
+                console.error('Error fetching characters:', error);
+            }
+        };
+        getData();
     }, []);
 
     return (
@@ -45,14 +66,14 @@ function Character(){
             </section>
             <section id="planets">
                 <h2>Homeworld</h2>
-                <p>{Homeworld}</p>
+                <p><a href={`/planets/${Homeworld.id}`}>{Homeworld.name}</a></p>
             </section>
             <section id="films">
                 <h2>Films appeared in</h2>
                 <ul>
-                    {FilmsList.map((film) =>
-                        <li key={film.id}>{film.name}</li>
-                    )}
+                    {FilmsList.map((film) =>(
+                        <li key={film.id}><a href={`/film/${film.id}`}>{film.title}</a></li>
+                    ))}
                 </ul>
             </section>
         </>
