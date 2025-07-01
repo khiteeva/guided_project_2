226889f923fff7909ab048a0c14261d7b6614ac5
Cecil <cecilho2486@gmail.com>
2025-07-01 13:36:10 -0400

    removed logo imports

--- frontend/guided-project-2/src/App.jsx
+++ frontend/guided-project-2/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import CharacterList from './components/CharacterList';
 import Character from './components/Character';
